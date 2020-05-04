63c62be2ae21b9bf6abcd9b63ae065424b853126
Kevin Gagan Serrano <kevgagser@gmail.com>
2020-05-03 18:52:48 -0700

    Add helper functions for adding message prefix and code formatting.

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
 
+function _addPrefix(msg) {
+  return `${prefix}${msg}`
+}
+
+function _codeStyle(msg) {
+  return '```' + msg + '```'
+}
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-  if (message.content === `${prefix}welcome`) {
+  if (message.content === _addPrefix('welcome')) {
     // send back "Pong." to the channel the message was sent in
 	    message.channel.send('...come');
-  } else if (message.content === `${prefix}help`) {
+  } else if (message.content === _addPrefix('help')) {
     // retrieve the help documents
-    message.channel.send('```!welcome - Timmy will say hello!```')
+    message.channel.send(_codeStyle('!welcome - Timmy will say hello!'))
   }
 });
 
