f2d33828b101c4f134ecae5777904fc6b4afd34d
Kevin Gagan Serrano <kevgagser@gmail.com>
2020-05-03 18:26:02 -0700

    Added the help command.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json')
+const { prefix, token } = require('./config.json');
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-  if (message.content === '!welcome') {
-	   // send back "Pong." to the channel the message was sent in
+  if (message.content === `${prefix}welcome`) {
+    // send back "Pong." to the channel the message was sent in
 	    message.channel.send('...come');
+  } else if (message.content === `${prefix}help`) {
+    // retrieve the help documents
+    message.channel.send('```!welcome - Timmy will say hello!```')
   }
 });
 
-client.login(config.token);
+client.login(token);
