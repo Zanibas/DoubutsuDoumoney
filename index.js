de024f30e11fdf2d9f18be15c782a4d83bd7e804
Kevin Serrano <kevgagser@gmail.com>
2020-05-08 18:56:33 -0700

    Linted and modified a bracket rule.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
 
 function _addPrefix(msg) {
-  return `${prefix}${msg}`
+	return `${prefix}${msg}`;
 }
 
 function _codeStyle(msg) {
-  return '```' + msg + '```'
+	return '```' + msg + '```';
 }
 
 client.once('ready', () => {
@@ -15,13 +15,13 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content === _addPrefix('welcome')) {
-    // send back "Pong." to the channel the message was sent in
-	    message.channel.send('...come');
-  } else if (message.content === _addPrefix('help')) {
-    // retrieve the help documents
-    message.channel.send(_codeStyle('!welcome - Timmy will say hello!'))
-  }
+	if (message.content === _addPrefix('welcome')) {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('...come');
+	} else if (message.content === _addPrefix('help')) {
+		// retrieve the help documents
+		message.channel.send(_codeStyle('!welcome - Timmy will say hello!'));
+	}
 });
 
 client.login(token);
