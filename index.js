c1eccebdbda9556b927ee202ac50df0780cb3be9
Kevin Serrano <kevgagser@gmail.com>
2020-05-15 17:18:49 -0700

    Beginning of express routing. Moved around some env variables. Learned about promises again.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const Discord = require('discord.js');
+const crypto = require('crypto');
+const express = require('express');
+const app = express();
+const port = 3000;
 const client = new Discord.Client();
-const { token } = require('./config.json');
+const {
+	discordToken,
+	twitterConsumerApiSecretKey,
+	discordScreenshotsChannelId,
+} = require('./config.json');
 const { _addPrefix, _codeStyle } = require('./util.js');
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Discord connection established.');
+	client.channels.fetch(discordScreenshotsChannelId).then(channel => channel.send('Timmy is Online!'));
 });
 
 client.on('message', message => {
@@ -17,4 +26,25 @@ client.on('message', message => {
 	}
 });
 
-client.login(token);
+client.login(discordToken);
+
+// app.get('/webhook/twitter', (req, res) => {
+// 	const crcToken = req.query.crc_token;
+// 	if (crcToken) {
+// 		const hash = crypto.createHmac('sha256', twitterConsumerApiSecretKey).update(crcToken).digest('base64');
+// 		res.status(200);
+// 		res.send({
+// 			response_token: 'sha256' + hash,
+// 		});
+// 	} else {
+// 		res.status(400);
+// 		res.send('Error: crc_token missing from request.');
+// 	}
+// });
+//
+// app.post('/webhook/twitter', (req, res) => {
+// 	console.log(req.body);
+// 	console.log(req, res);
+// });
+//
+// app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
